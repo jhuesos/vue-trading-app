c34314f09edc167706c9e9825655459509aba6e3
Jaime Vega <jhuesos@gmail.com>
2017-12-11 19:26:04 +0100

    Remove everything

--- src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { DB_URL } from './config';
-
-// Set dbConnection defaults
-export const dbConnection = axios.create();
-dbConnection.defaults.baseURL = DB_URL;
-dbConnection.defaults.headers.common['Content-Type'] = 'application/json';
--- src/config.js
+++
@@ -1,3 +0,0 @@
-// TODO: this should be configurable
-export const DB_URL = 'https://stock-app-2806b.firebaseio.com/';
-export const STORAGE_KEY = 'PORTFOLIO_ID';
